2620edf65558a0718a810bcf1415227bd93d5e85
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-27 01:47:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,31 +11,32 @@ parse_git_branch() {
 echo "Adding customizations to ~/.bashrc..."
 
 # Adding theme
-if grep -i "LS_COLOR" ~/.bashrc then
-    sudo sed '/LS_COLORS/c\export LS_COLORS='di=35;1:fi=33:ex=36;1'' ~/.bashrc
+if grep -i "LS_COLORS" ~/.bashrc; then
+    sudo sed -i '/LS_COLORS/c\export LS_COLORS="di=35;1:fi=33:ex=36;1"' ~/.bashrc
 else
     sudo sed -i '7i\
-export LS_COLORS='di=35;1:fi=33:ex=36;1'' ~/.bashrc
+export LS_COLORS="di=35;1:fi=33:ex=36;1"' ~/.bashrc
 fi
 
 # PS1
-if grep -i "PS1" ~/.bashrc then
-    sudo sed -i '/PS1/c\export PS1='\[\033[0;30m\]@\h\n\[\033[01;34m\]\[\033[01;35m\]\u\[\033[00m\] \[\033[01;37m\]<> \[\033[01;34m\]\w\[\033[01;33m\]$(parse_git_branch)\[\033[00m\] '' ~/.bashrc
+if grep -i "PS1" ~/.bashrc; then
+    sudo sed -i '/PS1/c\export PS1="\[\033[0;30m\]@\h\n\[\033[01;34m\]\[\033[01;35m\]\u\[\033[00m\] \[\033[01;37m\]<> \[\033[01;34m\]\w\[\033[01;33m\]$(parse_git_branch)\[\033[00m\]"' ~/.bashrc
 fi
 
-echo -e "\033]10;#9F9F9F;#FFFFFF\033\\" >> ~/.bashrc # Set text color (white) and background (dark grey)
-echo -e "\033]11;#000000\033\\" >> ~/.bashrc # Set background color (black)
+# Set text color and background
+echo -e "\033]10;#9F9F9F;#FFFFFF\033\\" | sudo tee -a ~/.bashrc > /dev/null # Set text color (white) and background (dark grey)
+echo -e "\033]11;#000000\033\\" | sudo tee -a ~/.bashrc > /dev/null # Set background color (black)
 
 # Finished :3
 echo "Bash aesthetic updated!"
 
-# Sourcing .bashrc
+# Prompt for sourcing .bashrc
 echo "Sourcing .bashrc..."
 source ~/.bashrc
 
 # Cleaning up...
 cd ~
-read -p "Would you like to delete the theme folder [y/n]" yn
+read -p "Would you like to delete the theme folder [y/n]: " yn
 if [[ $yn == "y" ]]; then
     rm -rf BeverageTheme
 else
