fe9a2b773cc08a9bf2e1cefde5c3b46d9da59f3a
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-27 01:51:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,10 +38,11 @@ echo "Sourcing .bashrc..."
 source ~/.bashrc
 
 # Cleaning up...
-cd ~
 read -p "Would you like to delete the theme folder [y/n]: " yn
 if [[ $yn == "y" ]]; then
     rm -rf BeverageTheme
 else
     echo "Skipping clean up"
 fi
+
+cd ~
